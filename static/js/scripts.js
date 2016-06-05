7f8709057b69665d281f9ad3849f47edf2439d9b
rindi <rindi@mail.sfsu.edu>
2016-06-04 22:27:48 -0700

    Changed map default location to SF

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -8,8 +8,8 @@ console.log('hello world');
 function initMap() {
   // Create a map object and specify the DOM element for display.
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
+    center: {lat: 37.7749, lng: -122.4194},
     scrollwheel: false,
-    zoom: 8
+    zoom: 12
   });
 }
