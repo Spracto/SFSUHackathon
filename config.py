5ae4d2d986a2a4e89e27f5bc668a153ec7ca2cb0
rindi <rindi@mail.sfsu.edu>
2016-06-04 22:15:14 -0700

    Added query test

--- config.py
+++ config.py
@@ -9,22 +9,25 @@ import sys
 
 try:
     conn = psycopg2.connect("dbname='sfsuhackathon' user='postgres' host='localhost' password='#2002Civic'")
+    # conn = psycopg2.connect("dbname='sfhackathon' user='rushabindi' host='localhost' password='password'")
     print "heyyyy that pretty cool"
 except:
     print "I am unable to connect to the database"
 
 cur = conn.cursor()
+curs = conn.cursor()
 
 try:
-    SQL = "SELECT address FROM hackathon WHERE address LIKE (%s);"
+    curs.execute("Select * FROM hackathon")
+    colnames = [desc[0] for desc in curs.description]
+    print colnames
+    
+    SQL = "SELECT * FROM hackathon WHERE address LIKE (%s);"
     data = ("255 RED%",)
     cur.execute(SQL, data)
-    # cur.execute("SELECT address FROM hackathon WHERE address LIKE (%s)", (""))
-    # cur.execute("""SELECT address FROM hackathon WHERE address LIKE '255 RED ROCK WY%'""")
-    # cur.query("""SELECT address FROM hackathon WHERE address LIKE ''""")
+
 except:
     print "cannot execute"
 
 rows = cur.fetchall()
-for row in rows:
-    print "  ", row[0]
+print rows
